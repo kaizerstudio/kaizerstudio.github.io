13480b04820b3e5aeccc2442a048df709fe24fd7
kaizerstudio <truongsondotcom@outlook.com>
2021-02-05 19:42:05 +0700

    main

--- src.js
+++ src.js
@@ -3,3 +3,9 @@ console.log("This vadim is working!");
 document.getElementById("btn1").onclick = function () {
     console.log("Button 1 worked.");
 }
+document.getElementById("btn2").onclick = function () {
+    console.log("Button 2 worked.");
+}
+document.getElementById("btn3").onclick = function () {
+    console.log("Button 3 worked.");
+}
