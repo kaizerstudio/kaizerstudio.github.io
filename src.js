744d8b76f95796e152510e9ba3a4f4b7d0218797
kaizerstudio <truongsondotcom@outlook.com>
2021-02-05 19:41:31 +0700

    main

--- src.js
+++ src.js
@@ -1,2 +1,5 @@
 // wtf is going on here?
-console.log("This vadim is working!")
+console.log("This vadim is working!");
+document.getElementById("btn1").onclick = function () {
+    console.log("Button 1 worked.");
+}
